35bbb62447590f881026850ef67a63f72492b5ff
dhanunjay20 <dhanunjay@hiresoftsolutions.com>
2025-11-21 11:27:44 +0530

    initial commit

---
+++ bidding/src/test/java/com/event/bidding/BiddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.bidding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
